e7984fea50a20586ee1511e0d99d552d002de804
Andri277 <99749611+Andri277@users.noreply.github.com>
2022-03-17 20:28:07 +0700

    Update day16_java_substring_comparisons.java

--- day16_java_substring_comparisons.java
+++ day16_java_substring_comparisons.java
@@ -1,18 +1,29 @@
 import java.util.Scanner;
 
-import java.io.*;
-import java.util.*;
-import java.text.*;
-import java.math.*;
-import java.util.regex.*;
-
 public class Solution {
 
+    public static String getSmallestAndLargest(String s, int k) {
+        String smallest = "";
+        String largest = "";
+        
+        java.util.List<String> a = new java.util.ArrayList<>();
+
+        for(int i=0;i<s.length()-k+1;i++){
+                a.add(s.substring(i,i+k));   
+        }
+        java.util.Collections.sort(a);
+         smallest = a.get(0);
+         largest = a.get(a.size()-1);
+        return smallest + "\n" + largest;
+    }
+
+
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String S = in.next();
-        int start = in.nextInt();
-        int end = in.nextInt();
-        System.out.println(S.substring(start,end));
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+      
+        System.out.println(getSmallestAndLargest(s, k));
     }
 }
