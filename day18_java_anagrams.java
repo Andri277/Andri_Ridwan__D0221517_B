f6044533a8490e94e0b908855b1b024fd87e749f
Andri277 <99749611+Andri277@users.noreply.github.com>
2022-03-17 20:37:35 +0700

    Update day18_java_anagrams.java

--- day18_java_anagrams.java
+++ day18_java_anagrams.java
@@ -2,42 +2,40 @@ import java.util.Scanner;
 
 public class Solution {
 
-    static boolean isAnagram(String a, String b){
+    static boolean isAnagram(String s1, String s2) {
         // Complete the function
-        boolean result = true;
-        if(a==null||b==null||a.equals("")||b.equals("")){
-            throw new IllegalArgumentException();            
-        }
-        if(a.length()!=b.length()){
-            result = false;
-        }else{
-            a = a.toLowerCase();
-            b = b.toLowerCase();
-            
-            java.util.Map<Character, Integer> mapA = new java.util.HashMap<>();
-            java.util.Map<Character, Integer> mapB = new java.util.HashMap<>();
-            
-            for(int i=0;i<a.length();i++){
-                char charA = a.charAt(i);
-                char charB = b.charAt(i);
-                // untuk karakter A
-                if(!mapA.containsKey(charA)){
-                    mapA.put(charA, 1);
-                }else{
-                    Integer frekuensi = mapA.get(charA);
-                    mapA.put(charA,++frekuensi);
-                }
-                
-                if(!mapB.containsKey(charB)){
-                    mapB.put(charB, 1);
-                }else{
-                    Integer frekuensi = mapB.get(charB);
-                    mapB.put(charB,++frekuensi);
-                }   
+        s1=s1.toLowerCase();
+        s2=s2.toLowerCase();
+        
+        if(s1.length()==s2.length())
+
+        {
+            int[] a = new int[256];
+            int[] b = new int[256];
+            for (int i = 0; i < s1.length(); i++) {
+                a[(int) s1.charAt(i)] += 1;
+                b[(int) s2.charAt(i)] += 1;
             }
-        
-        
-            for(java.util.Map.Entry<Character, Integer> elemenA : mapA.entrySet()){
-                char keyA = elemenA.getKey();
-                int valueA = elemenA.getValue();
-            
+            for (int i = 0; i < 256; i++) {
+                if (a[i] != b[i])
+                    return false;
+
+            }
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+    
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+    }
+}
